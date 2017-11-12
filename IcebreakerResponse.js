bd0d956574f5d9ed6888bd266d826597b661aeb4
Jennifer Rhees <redswinglinestapler23@gmail.com>
2017-11-12 21:09:11 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -6,4 +6,8 @@ IcebreakerResponse.BatchCreate = function(emails) {
   return emails.map(function(email) {
     return new IcebreakerResponse(email);
   });
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 849d41ffd507ff55ffc575626d4307857291a0b5
